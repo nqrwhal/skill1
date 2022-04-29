2ac6bbafec19bf2f6fce7307895b47e13346dab0
nqrwhal <yum011@ucsd.edu>
2022-04-28 20:45:06 -0700

    master

--- Skill.java
+++ Skill.java
@@ -1,7 +1,7 @@
 class Skill {
 
     public static int greaterthan100(int args) {
-        if (args >= 100) {
+        if (args > 100) {
             return 1;
         } else {
             return 0;
